53141bb9d3847a4b14f4178fefd8ebbc7e07e165
kamrulcse404 <kamrul.h01960@gmail.com>
2024-05-10 18:45:50 +0600

    reading and setting query string

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,3 +1,4 @@
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -9,6 +10,13 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
+  const { id } = useParams();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
@@ -19,7 +27,14 @@ function City() {
 
   const { cityName, emoji, date, notes } = currentCity;
 
-  return <p>City</p>;
+  return (
+    <>
+      <h1>City: {id}</h1>
+      <p>
+        Position: {lat}, {lng}
+      </p>
+    </>
+  );
 
   // return (
   //   <div className={styles.city}>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import styles from "./Map.module.css";
+import { useParams, useSearchParams } from "react-router-dom";
 
 const Map = (props) => {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h1>
+        Position : {lat}, {lng}
+      </h1>
+      <button
+        onClick={() =>
+          setSearchParams({
+            lat: 23,
+            lng: 45,
+          })
+        }
+      >
+        Change position
+      </button>
+    </div>
+  );
 };
 
 export default Map;
